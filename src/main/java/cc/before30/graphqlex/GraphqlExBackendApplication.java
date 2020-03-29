77c2ef80d23f754cf3fb2bfd38ca84ec74ce2ea2
Joseph Yang <before30@gmail.com>
2020-03-29 23:40:03 +0900

    Add baseline

---
+++ src/main/java/cc/before30/graphqlex/GraphqlExBackendApplication.java
@@ -0,0 +1,18 @@
+package cc.before30.graphqlex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * GraphqlExBackendApplication
+ *
+ * @author before30
+ * @since 2020/03/29
+ */
+
+@SpringBootApplication
+public class GraphqlExBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlExBackendApplication.class, args);
+    }
+}
